8e11cfbd64478f20473ad877a32f4dd46b55a554
Moses-254-Mugo <moses.mugo@student.moringaschool.com>
2021-09-29 23:31:07 +0300

    Add service feature-service

--- src/app/features-service.service.ts
+++ src/app/features-service.service.ts
@@ -1,17 +1,18 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import {HttpClient}from '@angular/common/http';
 import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
-export class FieldsServiceService {
+export class FeaturesServiceService {
   _URL = 'https://opendata.arcgis.com/datasets/11388debff0f4b61b9f60ba81cf569c8_0/FeatureServer/0/query?where=1%3D1&outFields=*&outSR=4326&f=json';
-  
+
+
   token = `11388debff0f4b61b9f60ba81cf569c8_0`
 
-  constructor( public http: HttpClient) { }
-  getFields(searchTerm: string): Observable<any>{
-    return this.http.get(this._URL + searchTerm + this.token)
-  }
+  constructor(public http: HttpClient) { }
+   getFeatures(searchTerm: string): Observable<any>{
+     return this.http.get(this._URL + searchTerm + '/features'+ this.token);
+   }
 }
