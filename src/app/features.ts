d511445575c8e2665c8e3653eaae8268a67e829f
Moses-254-Mugo <moses.mugo@student.moringaschool.com>
2021-09-29 23:05:47 +0300

    Add component field-service

--- src/app/features.ts
+++ src/app/features.ts
@@ -1,3 +1,17 @@
 export class Features {
-    
+    HUDUMA_CENTER !: string;
+    TOTAL_NUMBER !: number;
+    YEAR !: Date;
+    OBJECT !: number; 
+
+
+    constructor(HUDUMA_CENTER: string, TOTAL_NUMBER: number,YEAR: Date,  OBJECT: number){
+        this.HUDUMA_CENTER = HUDUMA_CENTER;
+        this.TOTAL_NUMBER = TOTAL_NUMBER
+        this.YEAR = YEAR;
+        this.OBJECT = OBJECT;
+        
+    }
+
+
 }
