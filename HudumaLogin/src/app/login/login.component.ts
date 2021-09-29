e847d13fe3ce6cdd345bffda1d748e2f6f9aed4f
othienoJoe <bryo094@gmail.com>
2021-09-29 17:25:15 +0300

    made the login page responsive

--- HudumaLogin/src/app/login/login.component.ts
+++ HudumaLogin/src/app/login/login.component.ts
@@ -1,5 +1,7 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -9,12 +11,30 @@ import { FormBuilder, FormGroup } from '@angular/forms';
 export class LoginComponent implements OnInit {
 
   public loginForm !: FormGroup
-  constructor(private formBuilder : FormBuilder) { }
+  constructor(private formBuilder : FormBuilder, private http : HttpClient, private router : Router) { }
 
   ngOnInit(): void {
     this.loginForm = this.formBuilder.group({
       email: [''],
-      password: ['']
+      password: [''],
+      remember: [""]
+    })
+  }
+  login() {
+    this.http.get<any>("http://localhost:3000/signupUsers")
+    .subscribe(res=> {
+      const user = res.find((a:any) => {
+        return a.email === this.loginForm.value.email && a.password === this.loginForm.value.password
+      });
+      if(user) {
+        alert("Login Successful!");
+        this.loginForm.reset();
+        this.router.navigate(['home'])
+      }else {
+        alert("User Not Found!");
+      }
+    },err=> {
+        alert("Something went wrong!")
     })
   }
 
